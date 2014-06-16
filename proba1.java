05cbee580a92f1a673f0e3394391eb8babb1c8b4
Trololoshe4ki <vladimir4862@gmail.com>
2014-06-16 23:14:56 +0400

    Поправил оформление
    Чуть чуть поправил оформление, ну и имена переменны�
    и их доступность

--- proba1.java
+++ proba1.java
@@ -1,131 +1,101 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Proba1 {
-  public static ArrayList<ArrayList> wood = new ArrayList<ArrayList> ();
-   // public static ArrayList secondLvl = new ArrayList ();
-  public static String check;// Переменная для проверки того что ввёл пользователь
-  public static int Vertex; //Переменная чтобы новый корень был +1 от Предыдущего
+public class Proba1 { 
+    private static ArrayList<ArrayList> wood = new ArrayList<ArrayList> ();
+    private static int Vertex; //Переменная чтобы новый корень был +1 от Предыдущего
 
-  public static void main (String [] args) {
-    Scanner sc = new Scanner (System.in);
-    System.out.println("command");
-      check = sc.nextLine();
-      if(check.equals("command")) {
-           // sc.nextLine
-          //if (sc.hasNextInt()) {
-          command();
-      }else {
-        System.out.println("Errormain");
-    }
-  }
-  public static void command () {
-    Scanner sc = new Scanner (System.in);
-    System.out.println("command: addVertex; printVertex; deleteVertex; printWood");
-    check = sc.nextLine();
-    if(check.equals("addVertex")) {
-            System.out.println("Father Vertex");
-            
-            int check = sc.nextInt();      
-            if(check<=wood.size()) {
-            System.out.println("executed");
-            addVertex(check);
-            } else {
-            System.out.println("Error addVertex");
+    public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("command");
+            String input = sc.nextLine();
+            if(input.equals("command")) {
             command();
-            }
-         // System.out.println("Name Vertex");
-          //addVertex();
-    } else if(check.equals("printVertex")) {
-           System.out.println("Number");
-           int check = sc.nextInt();
-           if (check <= wood.size()) {
-              System.out.println("executed");
-              printVertex(check);    
-           } else { 
-              System.out.println("Error printVertex");
-              command();
-          }
-    } else if(check.equals("deleteVertex")) {
-          System.out.println("Number");
-          int check = sc.nextInt();
-          if (check <= wood.size()) {
-            System.out.println("executed");
-            deleteVertex(check);
-          } else {
-             System.out.println("Error Number");
-             command();
-          }
-     } else if(check.equals("Test")) {
-          System.out.println("Number");
-          int check = sc.nextInt();
-          if (check <= wood.size()) {
-            System.out.println("executed");
-            Test(check);
-          } else {
-             System.out.println("Error Number");
-             command();
-          }
-    } else if(check.equals("printWood")) {
-       printWood();   
-    } else {
-      System.out.println("Errorcommand");
-      command();
+            } else {
+            System.out.println("Errormain");
+           }
     }
-  }
+    private static void command () {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("command: addVertex, printVertex, deleteVertex, printWood");
+        String input = sc.nextLine();
+            if(input.equals("addVertex")) {
+                System.out.println("Father Vertex");
+                int value = sc.nextInt();      
+                if(value <= wood.size()) {
+                    System.out.println("executed");
+                    addVertex(value);
+                } else {
+                    System.out.println("Error addVertex");
+                    command();
+                }
+         
+            } else if(input.equals("printVertex")) {
+                System.out.println("Number");
+                int value = sc.nextInt();
+                if (value <= wood.size()) {
+                    System.out.println("executed");
+                    printVertex(value);    
+                } else { 
+                    System.out.println("Error printVertex");
+                    command();
+                }
+            } else if(input.equals("deleteVertex")) {
+                System.out.println("Number");
+                int value = sc.nextInt();
+                if (value <= wood.size()) {
+                    System.out.println("executed");
+                    deleteVertex(value);
+                } else {
+                    System.out.println("Error Number");
+                    command();
+                }
+            /*} else if(input.equals("Test")) {
+                System.out.println("Number");
+                int value = sc.nextInt();
+                if (value <= wood.size()) {
+                    System.out.println("executed");
+                    Test(value);
+                } else {
+                    System.out.println("Error Number");
+                    command();
+                }
+            */
+            } else if(input.equals("printWood")) {
+                printWood();   
+            } else {
+                System.out.println("Errorcommand");
+                command();
+            }
+    }
   
-  public static void addVertex (int FatherVertex) {
-    Vertex++;
-    int NumberVertex = Vertex;
-    ArrayList Vertex = new ArrayList ();
+    private static void addVertex (int FatherVertex) {
+        Vertex++;
+        int NumberVertex = Vertex;
+        ArrayList Vertex = new ArrayList ();
+        Vertex.add(0, null);
+        Vertex.set(0, FatherVertex);
+        Vertex.add(1,NumberVertex);
+        wood.add(Vertex);
+        command();
+    }
+  
+    private static void printWood() {         
+        System.out.println(wood);
+        command ();
+    }
+    private static void printVertex(int numberVertex) {
+        System.out.println(wood.get(numberVertex));
+        command ();
     
-    Vertex.add(0, null);
-    Vertex.set(0, FatherVertex);
-    Vertex.add(1,NumberVertex);
-    wood.add(Vertex);
-    command();
-  }
-  
-  public static void printWood() {         
-    System.out.println(wood);
-    command ();
-  }
-  public static void printVertex(int numberVertex) {
-    System.out.println(wood.get(numberVertex));
-    command ();
+    }  
+    private static void deleteVertex(int NumberVertex) {
+        wood.remove(NumberVertex);
+        command();
+    }
     
-  }  
-  public static void deleteVertex(int NumberVertex) {
-    wood.remove(NumberVertex);
-    command();
-  }
-  //Не работает, надо посмотреть
-  public static void Test (int numberVertex) {
-    System.out.println(wood.get(numberVertex.get(1)));
-    command();
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
- 
- 
- 
- 
- }
- 
- 
- 
+    //Не работает, надо посмотреть
+    //public static void Test (int numberVertex) {
+     //   System.out.println(wood.get(numberVertex.get(1)));
+    //    command();
+    //}
+} 
