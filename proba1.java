b3d3aa46dc490dc6c2dc6de35a7f320f43a83450
Trololoshe4ki <vladimir4862@gmail.com>
2014-06-23 22:46:59 +0400

    1
    1

--- proba1.java
+++ proba1.java
@@ -3,25 +3,35 @@ import java.util.Scanner;
 public class Proba1 { 
     private static ArrayList<ArrayList> wood = new ArrayList<ArrayList> ();
     private static int Vertex; //Переменная чтобы новый корень был +1 от Предыдущего
-
+    private static Scanner sc = new Scanner (System.in);
     public static void main (String [] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("command");
-            String input = sc.nextLine();
-            if(input.equals("command")) {
-            command();
-            } else {
-            System.out.println("Errormain");
-           }
-    }
-    private static void command () {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("command: addVertex, printVertex, deleteVertex, printWood");
         String input = sc.nextLine();
-            if(input.equals("addVertex")) {
+            if (input.equals("command")) {
+                System.out.println("executed");
+                command();
+            } else {
+                System.out.println("Errormain");
+            }
+    }
+   
+    private static void command() {
+        System.out.println("command: addVertex, printVertex, deleteVertex, printWood");
+            String input = sc.nextLine();
+             while (input.equals("addVertex")) {
+                System.out.println ("2");
+                addVertex();
+             }
+        
+        //for (String input = sc.nextLine(); input.equals("addVertex");System.out.println("1")) {
+        //        System.out.println("2");
+        //       addVertex();
+        //}
+            /*if (input.equals("addVertex")) {
                 System.out.println("Father Vertex");
                 int value = sc.nextInt();      
-                if(value <= wood.size()) {
+                if (value <= wood.size()) {
                     System.out.println("executed");
                     addVertex(value);
                 } else {
@@ -49,7 +59,7 @@ public class Proba1 {
                     System.out.println("Error Number");
                     command();
                 }
-            /*} else if(input.equals("Test")) {
+            } else if(input.equals("Test")) {
                 System.out.println("Number");
                 int value = sc.nextInt();
                 if (value <= wood.size()) {
@@ -59,26 +69,36 @@ public class Proba1 {
                     System.out.println("Error Number");
                     command();
                 }
-            */
+            
             } else if(input.equals("printWood")) {
                 printWood();   
             } else {
                 System.out.println("Errorcommand");
                 command();
             }
+            */
     }
-  
-    private static void addVertex (int FatherVertex) {
-        Vertex++;
-        int NumberVertex = Vertex;
-        ArrayList Vertex = new ArrayList ();
-        Vertex.add(0, null);
-        Vertex.set(0, FatherVertex);
-        Vertex.add(1,NumberVertex);
-        wood.add(Vertex);
-        command();
+    private static void peint() {
+        for( int i = 1; i < 10; ++i) {
+        System.out.print (Vertex);
+        }
     }
-  
+    private static void addVertex () {
+        System.out.println("Fater Vertex");
+        int FatherVertex = sc.nextInt();
+        if (FatherVertex <= wood.size()) {
+            Vertex++;
+            int NumberVertex = Vertex;
+            ArrayList Vertex = new ArrayList ();
+          //Vertex.add(0, null);
+            Vertex.add(0, FatherVertex);
+            Vertex.add(1, NumberVertex);
+            wood.add(Vertex);
+        } else {
+            System.out.println("Error Fater Vertex");
+        }
+    }
+  /*
     private static void printWood() {         
         System.out.println(wood);
         command ();
@@ -98,4 +118,5 @@ public class Proba1 {
      //   System.out.println(wood.get(numberVertex.get(1)));
     //    command();
     //}
+    */
 } 
