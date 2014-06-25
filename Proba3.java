6a3d4684826db580d220f66fd0c51aaf4a61ade6
Trololoshe4ki <vladimir4862@gmail.com>
2014-06-26 00:19:58 +0400

    удаление вершины, добавелние вершины,

--- Proba3.java
+++ Proba3.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Proba3 {
-    private static ArrayList <ArrayList> tree = new ArrayList <ArrayList> ();
+    private static ArrayList <ArrayList <Integer>> tree = new ArrayList <ArrayList<Integer>> ();
     private static Scanner sc = new Scanner (System.in);
-    private static int numberVertex; // для создание новых Vertex
+    private static int numberVertex = -1; // для создание новых Vertex
     public static void main (String [] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Hi");
@@ -20,6 +20,8 @@ public class Proba3 {
                 printTree();
             } else if (input.equals("deleteVertex")) {
                 deleteVertex();
+            } else if (input.equals("test")) {
+                test ();
             } else {
                 System.out.println("error");
             }
@@ -32,11 +34,20 @@ public class Proba3 {
     private static void addVertex() {
         System.out.println("FatherVertex");
         int FatherVertex = sc.nextInt();
-        if (FatherVertex <= tree.size()) {
+        if (FatherVertex < tree.size()) {
             numberVertex++;
-            ArrayList Vertex = new ArrayList ();
+            ArrayList <Integer> Vertex = new ArrayList <Integer> ();
             Vertex.add(0, numberVertex);
             Vertex.add(1, FatherVertex);
+            Vertex.add(2, 123456789);
+            tree.add(Vertex);
+            tree.get(FatherVertex).add(numberVertex);
+        } else if (FatherVertex == 0) {
+          numberVertex++;
+            ArrayList <Integer> Vertex = new ArrayList <Integer> ();
+            Vertex.add(0, numberVertex);
+            Vertex.add(1, FatherVertex);
+            Vertex.add(2, 123456789);
             tree.add(Vertex);
         } else{
             System.out.println("Error Fater Vertex");
@@ -46,23 +57,28 @@ public class Proba3 {
     private static void deleteVertex() {
         System.out.println("numberVertex");
         int deleteNumber = sc.nextInt();
-        
-        tree.remove(deleteNumber);
-        for (int i = 0; i <= tree.size(); i++) {
-            ArrayList test = tree.get(i);
-            System.out.println(test);
-            //for (int a = test.get(1); a > deleteNumber;) {
-             //   int b = test.get(1);
-             //   b--;
-             //   test.add(1, b);
-            
-            
+        for (int i = deleteNumber--; i < tree.size(); i++) {
+            tree.get(i).set(0, i);
+        //    for (i = 3; i < tree.get(deleteNumber).size(); i++) {
+      //          int Branch = tree.get(deleteNumber).get(i);
+    //            tree.remove(Branch);
         }
-        printTree ();
+        tree.remove(deleteNumber);
     }
-     
+    
+    
+   /* private static void deleteBranch(deleteNumber) {
+         for (int i = deleteNumber--; i < tree.size(); i++) {
+            tree.get(i).set(0, i);
+            for (i = 3; i < tree.get(deleteNumber).size(); i++) {
+                int Branch = tree.get(deleteNumber).get(i);
+                tree.remove(Branch);
+                
+    */
     private static void printTree() {
-        System.out.println(tree);
+        for (int i = 0; i < tree.size(); i++) {
+            System.out.println(tree.get(i));
+        }
     }
     
     private static void printVertex() {
@@ -73,5 +89,10 @@ public class Proba3 {
         } else {
             System.out.println("does not have a vertex number " +printNumber );
         }
+    }
+    private static void test () {
+      int i = sc.nextInt();
+      int a = tree.get(i).get(0);
+      System.out.println(a);  
     }
 }    
