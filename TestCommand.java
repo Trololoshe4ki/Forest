f7ff521f373b5dc3f6a73919826f0f132170000d
Trololoshe4ki <vladimir4862@gmail.com>
2014-08-04 14:41:24 +0400

    ,

--- TestCommand.java
+++ TestCommand.java
@@ -14,10 +14,16 @@ import java.util.Scanner;
  class Invoker {
     private Command addVertex;
     private Command deleteVertex;
+    private Command printTree;
+    private Command printVertex;
+    private Command aR;
     
-    public Invoker (Command addVert, Command deleteVert) {
+    public Invoker (Command addVert, Command deleteVert, Command printT, Command printVert, Command aR) {
         this.addVertex = addVert;
         this.deleteVertex = deleteVert;
+        this.printTree = printT;
+        this.printVertex = printVert;
+        this.aR = aR;
     }
     
     public void addVertex() {
@@ -26,6 +32,16 @@ import java.util.Scanner;
     public void deleteVertex() {
         deleteVertex.execute();
     }
+    public void printTree() {
+        printTree.execute();
+    }
+    public void printVetrex () {
+        printVertex.execute(); 
+    }
+    public void aR () {
+        aR.execute();
+    }
+        
 }
 // Receiver 
 
@@ -54,8 +70,64 @@ import java.util.Scanner;
             System.out.println("Error Fater Vertex");
         }
     }
-    public void deleteVertex (int deleteNumber) {
-       if (TestCommand.tree.get(deleteNumber).size() == 3) {
+    
+    public void printTree() {
+        for (int i = 0; i < TestCommand.tree.size(); i++) {
+            System.out.println(TestCommand.tree.get(i));
+        }
+    }
+    public void printVertex() {
+        System.out.println("Number Vertex");
+        int printNumber = TestCommand.sc.nextInt();
+        if (printNumber <= TestCommand.tree.size()) {
+            System.out.println(TestCommand.tree.get(printNumber));
+        } else {
+            System.out.println("does not have a vertex number " +printNumber );
+        }
+   }
+    public void aR() {
+        TestCommand.IA.printTree();
+        System.out.println("NewRoot");
+        int newRoot = TestCommand.sc.nextInt();
+        for (int a = 0; a < TestCommand.tree.size(); a++) {
+            for (int c = 3; c < TestCommand.tree.get(a).size(); c++) {
+                if (TestCommand.tree.get(a).get(c) == newRoot) {
+                    TestCommand.tree.get(a).remove(c);
+                } else {                  
+                }
+            }
+            for (int b = 1; b < TestCommand.tree.get(a).size(); b++) {
+                if (newRoot > TestCommand.tree.get(a).get(b)) {
+                    int z = TestCommand.tree.get(a).get(b);
+                    z++;
+                    TestCommand.tree.get(a).set(b, z);
+                }else {
+                }
+            }
+        }
+        for (int a = 0, b = 2; a < TestCommand.tree.size(); a++) {
+             if (TestCommand.tree.get(a).get(b) == newRoot) {
+                TestCommand.tree.get(a).set(b, 0);
+             } else {
+             }
+        }
+        
+        TestCommand.tree.get(newRoot).set(1, 0);
+        TestCommand.tree.get(newRoot).set(2, 0);
+        TestCommand.tree.get(newRoot).add(3, 1);
+        TestCommand.tree.get(0).set(2,0);
+        TestCommand.IA.printTree();
+        TestCommand.tree.add(0, TestCommand.tree.get(newRoot));
+        System.out.println(newRoot);
+        TestCommand.tree.remove(newRoot+1);
+        System.out.println(newRoot+1);
+        TestCommand.IA.printTree();
+    }   
+    
+    public void deleteVertex () {
+        System.out.println("this is doesn't work!");
+      /* 
+      if (TestCommand.tree.get(deleteNumber).size() == 3) {
                 methodDelete(deleteNumber);
         } else if (TestCommand.tree.get(deleteNumber).size() > 3) {
                 for (int i = 3; i < TestCommand.tree.get(deleteNumber).size();) {
@@ -85,16 +157,52 @@ import java.util.Scanner;
             }
         }
      }
+     
+     */
 }
-//Command for add vertex
- class AddVertexCommand implements Command {
-    private Receiver thetest;
+}
+//Command for aR
+class ARCommand implements Command {
+    private Receiver theaR;
     
-    public AddVertexCommand (Receiver test) {
-        this.thetest = test;
+    public void aRCommand (Receiver aR) {
+        this.theaR = aR;
+    }
+    public void execute () {
+        theaR.aR();
+   }
+}
+//Command for printVertex
+class PrintVertexCommand implements Command {
+    private Receiver thePrintV;
+    
+    public void printVertexCommand (Receiver printV) {
+        this.thePrintV = printV;
     }
     public void execute() {
-        thetest.addVertex();
+        thePrintV.printVertex();
+    }
+}
+//Command for PrintTree
+class PrintTreeCommand implements Command {
+    private Receiver thePrintT;
+    
+    public  PrintTreeCommand (Receiver printT) {
+        this.thePrintT = printT;
+    }
+    public void execute() {
+        thePrintT.printTree();
+    }
+}
+//Command for add vertex
+class AddVertexCommand implements Command {
+    private Receiver theTest;
+    
+    public AddVertexCommand (Receiver test) {
+        this.theTest = test;
+    }
+    public void execute() {
+        theTest.addVertex();
     }
 }
 // Command for delete Vertex
@@ -106,7 +214,7 @@ class DeleteVertexCommand implements Command {
     }
     
     public void execute() {
-        theDelete.deleteVertex(deleteNumber); 
+        theDelete.deleteVertex(); 
     }
 }
     
@@ -120,176 +228,52 @@ public class TestCommand {
     public static void main(String[] args) {
                            
         
-         Receiver R = new Receiver();
+        Receiver R = new Receiver();
         
         Command CDel = new DeleteVertexCommand(R);
         Command CAdd = new AddVertexCommand(R);
+        Command CPrintT = new PrintTreeCommand(R);
+        Command CPrintV = new printVertexCommand(R);
+        Command CaR = new aRCommand(R);
         
-        Invoker IA = new Invoker(CAdd,CDel);
-        
-       
-        
-        
+        Invoker IA = new Invoker(CAdd,CDel,CPrintT,CPrintV,CaR);
         
         Scanner sc = new Scanner (System.in);
         System.out.println("Hi");
         
-       
-        
-        while (true) {
-            System.out.println("command: addVertex, printVertex, printTree, deleteVertex, rR or exit");
+    while (true) {
+            System.out.println("command: addVertex, printVertex, printTree, deleteVertex, aR(appointment root) or exit ");
             String input = sc.nextLine();
             if (input.equals("exit")) {
                 break;
-            } else if (input.equals("addVertex")) {
-                IA.addVertex();
-                
-                
-                
-            } else if (input.equals("printVertex")) {
-                printVertex();
-            } else if (input.equals("printTree")) {
-                printTree();
             } else if (input.equals("deleteVertex")) {
-                
-                
+                //System.out.println("numberVertex");
+              //  int deleteNumber = sc.nextInt();
+                IA.deleteVertex();
+           } else if (input.equals("addVertex")) {
+                IA.addVertex();
+            } else if (input.equals("printVertex")) {
+                IA.printVertex();
+            } else if (input.equals("printTree")) {
+                IA.printTree();
+            /*
+            } else if (input.equals("deleteVertex")) {
                 System.out.println("numberVertex");
                 int deleteNumber = sc.nextInt();
                 IA.deleteVertex(deleteNumber);
-                printTree();
-            } else if (input.equals("test")) {
-                test ();
-            } else if (input.equals("rR")) {
-                rR();
+            */
+           // } else if (input.equals("test")) {
+           //     test ();
+            } else if (input.equals("aR")) {
+                IA.aR();
             } else {
                 System.out.println("error");
             }
-         
-         
-         
          }
      }
-             
-    private static void addVertex() {
-        System.out.println("FatherVertex");
-        int FatherVertex = sc.nextInt();
-        if (FatherVertex < tree.size()) {
-            numberVertex++;
-            ArrayList <Integer> Vertex = new ArrayList <Integer> ();
-            Vertex.add(0, 123456789);
-            Vertex.add(1, numberVertex);
-            Vertex.add(2, FatherVertex);
-            tree.add(Vertex);
-            tree.get(FatherVertex).add(numberVertex);
-        } else if (FatherVertex == 0) {
-          numberVertex++;
-            ArrayList <Integer> Vertex = new ArrayList <Integer> ();
-            Vertex.add(0, 123456789);
-            Vertex.add(1, numberVertex);
-            Vertex.add(2, FatherVertex);
-            tree.add(Vertex);
-        } else{
-            System.out.println("Error Fater Vertex");
-        }
-        printTree();
-     }
+}
      
-     private static void deleteVertex(int deleteNumber) {
-        if (tree.get(deleteNumber).size() == 3) {
-                metodDelete(deleteNumber);
-        } else if (tree.get(deleteNumber).size() > 3) {
-                for (int i = 3; i < tree.get(deleteNumber).size();) {
-                    deleteVertex(tree.get(deleteNumber).get(i));
-                }
-                metodDelete(deleteNumber);
-        } else {
-                System.out.println("0_o");
-        }
-     }
-     
-     private static void metodDelete (int delete)   {
-        tree.remove(delete);
-        numberVertex--;
-        for (int a = 0; a < tree.size() ; a++) {
-            for (int b = 1; b < tree.get(a).size(); b++) {
-                if (tree.get(a).get(b) > delete) {
-                    int z = tree.get(a).get(b);
-                    z--;
-                    tree.get(a).set(b, z);
-                } else if (tree.get(a).get(b) == delete) {
-                    tree.get(a).remove(b);
-                    b--;
-                } else {
-                        
-                }
-            }
-        }
-     }
-     
-   private static void printTree() {
-        for (int i = 0; i < tree.size(); i++) {
-            System.out.println(tree.get(i));
-        }
-    }
-    
-    private static void printVertex() {
-        System.out.println("Number Vertex");
-        int printNumber = sc.nextInt();
-        if (printNumber <= tree.size()) {
-            System.out.println(tree.get(printNumber));
-        } else {
-            System.out.println("does not have a vertex number " +printNumber );
-        }
-    }
-    
-    private static void rR () {
-        printTree();
-        System.out.println("NewRoot");
-        int newRoot = sc.nextInt();
-        for (int a = 0; a < tree.size(); a++) {
-            for (int c = 3; c < tree.get(a).size(); c++) {
-                if (tree.get(a).get(c) == newRoot) {
-                    tree.get(a).remove(c);
-                } else {                  
-                }
-            }
-            for (int b = 1; b < tree.get(a).size(); b++) {
-                if (newRoot > tree.get(a).get(b)) {
-                    int z = tree.get(a).get(b);
-                    z++;
-                    tree.get(a).set(b, z);
-                }else {
-                }
-            }
-        }
-        for (int a = 0, b = 2; a < tree.size(); a++) {
-             if (tree.get(a).get(b) == newRoot) {
-                tree.get(a).set(b, 0);
-             } else {
-             }
-        }
-        
-        tree.get(newRoot).set(1, 0);
-        tree.get(newRoot).set(2, 0);
-        tree.get(newRoot).add(3, 1);
-        tree.get(0).set(2,0);
-        printTree();
-        tree.add(0, tree.get(newRoot));
-        System.out.println(newRoot);
-        tree.remove(newRoot+1);
-        System.out.println(newRoot+1);
-        printTree();
-    }
-    
-    private static void test () {
-      int i = sc.nextInt();
-      int a = tree.get(i).get(0);
-      System.out.println(a);  
-    }
-} 
 
-//}    
-    
     /*
     
     private static ArrayList <ArrayList <Integer>> tree = new ArrayList <ArrayList<Integer>> ();
